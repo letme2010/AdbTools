d5037963d4d032a3f5fc16a92a0c63165db90419
letme2010 <letme2010@gmail.com>
2014-04-12 13:57:13 +0800

    fix window devices empty line
    fix window devices empty line

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var APK_PATH = '/Users/chenzhipeng/Documents/workspace_eclipse/ThinkInDraw/bin/ThinkInDraw.apk';
+var APK_PATH = 'D:\\UCWorkspace\\97c5\\BrowserShell\\platform\\android\\bin\\97c5.apk';
 //var APK_PATH = '/Users/chenzhipeng/Downloads/uc.apk';
 
 var exec = require('child_process').exec;
@@ -8,7 +8,7 @@ exec('adb devices', function(error, stdout, stderr) {
 	var lines = stdout.split('\n');
 
 	for ( var i = 1; i < lines.length; i++) {
-		var line = lines[i];
+		var line = lines[i].trim();
 		if (0 == line.length) {
 			continue;
 		}
@@ -37,7 +37,7 @@ function installAPK(aDeviceSn, aStartAfterInstall) {
 
 		if (aStartAfterInstall) {
 			exec('adb -s ' + aDeviceSn
-					+ 'shell am start com.example.thinkindraw/.MainActivity',
+					+ 'shell am start com.UCMobile/.main.UCMobile',
 					function() {
 					});
 		}
