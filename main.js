798862346bca43cc28e170fbd9dd6673f434c006
letme2010 <letme2010@gmail.com>
2014-04-15 01:27:32 +0800

    feature : select device and send dab command
    feature : select device and send dab command

--- main.js
+++ main.js
@@ -24,34 +24,67 @@ exec('adb devices', function(error, stdout, stderr) {
 });
 */
 
+var DATA_KEY = {
+	DEVICE_WRAP : "DEVICE_WRAP"
+};
+
+function isMocking() {
+
+	try{
+		require;
+		return false;
+	} catch(e) {
+		return true;
+	}
+
+}
+
+var DEVICE_MOCK_LIST = ["192.168.56.101:5555", "192.168.56.102:5555", "ADKEK33K2K1K1KFF"];
+
+function getSelectedDevicesList(aCallback) {
+	if (aCallback) {
+
+	}
+}
+
 function getDevicesList(aCallback) {
 
 	if (aCallback) {
-		var exec = require('child_process').exec;
 
-		exec('adb devices', function(error, stdout, stderr) {
+		if (isMocking()) {
 
-		var lines = stdout.split('\n');
+			aCallback(DEVICE_MOCK_LIST);
 
-		var ret = [];
+		} else {
 
-		for ( var i = 1; i < lines.length; i++) {
+			var exec = require('child_process').exec;
 
-			var line = lines[i].trim();
+			exec('adb devices', function(error, stdout, stderr) {
 
-			if (0 == line.length) {
-				continue;
-			}
+			var lines = stdout.split('\n');
 
-			var devicesSn = line.split('device')[0];
+			var ret = [];
 
-			ret.push(devicesSn);
+			for ( var i = 1; i < lines.length; i++) {
 
-		};
+				var line = lines[i].trim();
 
-		aCallback(ret);
+				if (0 == line.length) {
+					continue;
+				}
 
-		});
+				var devicesSn = line.split('device')[0];
+
+				ret.push(devicesSn);
+
+			};
+
+			aCallback(ret);
+
+			});
+
+		}
+
 	}
 }
 
@@ -90,12 +123,252 @@ var Log = {
 
 };
 
-function main() {
-	getDevicesList(function(aList){
-		for(var i = 0; i < aList.length; i++) {
-			Log.i(i + ":" + aList[i]);
+var VIEW_ID = {
+	DEVICES_LIST_VIEW_ID : "device_list_view",
+	TERMINAL_ID : 'terminal',
+	CONSOLR_ID : 'console'
+};
+
+var CONFIG = {
+	DEVICES_CHECK_INTERVAL : 1000
+
+};
+
+function getDevicesListView() {
+	return $('#' + VIEW_ID.DEVICES_LIST_VIEW_ID);
+}
+
+function createDeviceBlock(aDeviceWrap) {
+	var _jqRet = $('<div class="deivce_block"></div>');
+
+	var _jqCheckBox = $('<input type="checkbox">');
+	var _jqDeviceSn = $('<span/>');
+
+	_jqDeviceSn.html(aDeviceWrap.mDeviceSn);
+	_jqCheckBox[0].checked = aDeviceWrap.mChecked;
+
+	_jqRet.append(_jqCheckBox);
+	_jqRet.append(_jqDeviceSn);
+
+	_jqRet.click(function(){
+
+		// Log.i("checkd:" + _jqCheckBox[0].checked);
+
+		var _isChecked = !_jqCheckBox[0].checked;
+		_jqCheckBox[0].checked = _isChecked;
+		if (_isChecked) {
+			gCheckedDeviceSnList.push(aDeviceWrap.mDeviceSn);
+		} else {
+			gCheckedDeviceSnList.remove(aDeviceWrap.mDeviceSn);
 		}
+
+	});
+
+	return _jqRet;
+}
+
+Array.prototype.contains = function(aObj) {
+
+	if (!aObj) {
+		return false;
+	}
+
+	for (var i = 0; i < this.length; i++) {
+
+		if ((undefined != aObj.equals) && (undefined != this[i].equals)) {
+			return this[i].equals(aObj);
+		} else if (aObj == this[i]) {
+			return true;
+		}
+
+	}
+	return false;
+};
+
+Array.prototype.remove = function(aObj) {
+	if (!aObj) {
+		return false;
+	}
+
+	for (var i = 0; i < this.length; i++) {
+
+		if ((undefined != aObj.equals) && (undefined != this[i].equals)) {
+			this.splice(i,1);
+			return true;
+		} else if (aObj == this[i]) {
+			this.splice(i,1);
+			return true;
+		}
+
+	}
+	return false;
+}
+
+var gDeviceWrapList = [];
+
+function DeviceWrap(aDeviceSn) {
+	this.mDeviceSn = aDeviceSn;
+	this.mChecked = true;
+
+	this.equals = function(aDeviceWrap) {
+		if (null == aDeviceWrap) {
+			return false;
+		}
+		return (this.mDeviceSn == aDeviceWrap.mDeviceSn);
+	};
+
+	this.toString = function() {
+		return {
+			deviceSn : this.mDeviceSn,
+			checked : this.mChecked
+		}.toString();
+	}
+}
+
+function addDevice(aDeviceWrap) {
+
+	if (gDeviceWrapList.contains(aDeviceWrap)) {
+		return;
+	}
+
+	gDeviceWrapList.push(aDeviceWrap);
+}
+
+function removeDevice(aDeviceWrap) {
+	if (gDeviceWrapList.contains(aDeviceWrap)) {
+		gDeviceWrapList.remove(aDeviceWrap);
+	}
+}
+
+function clearDevice() {
+	gDeviceWrapList = [];
+}
+
+function getTerminal() {
+	return $('#' + VIEW_ID.TERMINAL_ID);
+}
+
+function getConsole() {
+	return $('#' + VIEW_ID.CONSOLR_ID);
+}
+
+function log(aMsg) {
+
+	var _jqRet = $('<div></div>');
+	var _jqTime = $('<pre class="time_span"></pre>');
+	var _jqMsg = $('<pre class="log_span"></pre>');
+
+	_jqTime.html(new Date().toString());
+	_jqMsg.html(aMsg);
+
+	_jqRet.append(_jqTime);
+	_jqRet.append(_jqMsg);
+
+	getConsole().append(_jqRet);
+
+}
+
+function onSendCommandButtonClick() {
+	var text = getTerminal()[0].value.trim();
+
+	if (0 >= text.length) {
+		return;
+	}
+
+	if (0 == text.indexOf('adb')) {
+		getSelectedDevicesList(function(aList){
+			for (var i = 0; i < aList.length; i++){
+				var cmd = text.replace('adb', 'adb -s ' + aList[i]);
+				executeCommand(cmd, function(aMsg){
+					log(aMsg);
+				});
+			}
+		});
+	} else {
+
+		try{
+			executeCommand(text, function(aMsg){
+				log(aMsg);
+			});
+		}catch(e) {
+			log(e);
+		}
+
+	}
+
+	afterSendCommand();
+}
+
+function afterSendCommand(){
+	getTerminal()[0].value = "";
+}
+
+function executeCommand(aCmd, aCallback) {
+	var exec = require('child_process').exec;
+
+	exec(aCmd, function(error, stdout, stderr) {
+
+		if (aCallback) {
+			if (error) {
+				aCallback(stderr);
+			} else {
+				aCallback(stdout);
+			}
+		}
+
 	});
 }
 
-main();
+var gCheckedDeviceSnList = [];
+
+function queryCheckedStatus(aDeviceSn) {
+	return gCheckedDeviceSnList.contains(aDeviceSn);	
+}
+
+function notifyDeviceListChanged() {
+
+	getDevicesListView().empty();
+
+	for (var i = 0, len = gDeviceWrapList.length; i < len; i++) {
+		var deviceWrap = gDeviceWrapList[i];
+		deviceWrap.mChecked = queryCheckedStatus(deviceWrap.mDeviceSn);
+		var _jqDeviceBlock = createDeviceBlock(deviceWrap);
+		getDevicesListView().append(_jqDeviceBlock);
+	}
+}
+
+function testDeviceRemoved() {
+	DEVICE_MOCK_LIST.pop();
+}
+
+function testDeviceAdded() {
+	DEVICE_MOCK_LIST.push("localhost:9527");
+}
+
+function main() {
+
+	$('#' + VIEW_ID.TERMINAL_ID).bind('keydown', 'enter', function(ev){
+		if (ev.ctrlKey && (13 == ev.keyCode)) {
+			onSendCommandButtonClick();
+		}
+	});
+
+
+	setInterval(function(){
+
+		getDevicesList(function(list){
+
+			clearDevice();
+			notifyDeviceListChanged();
+
+			for (var i = 0, len = list.length; i < len; i++) {
+				var deviceWrap = new DeviceWrap(list[i]);
+				addDevice(deviceWrap);
+			}
+			notifyDeviceListChanged();
+
+		});
+
+	 }, CONFIG.DEVICES_CHECK_INTERVAL);
+
+}
---
+++ run.sh
@@ -0,0 +1,3 @@
+sudo killall node-webkit
+zip AdbTools.nw index.html package.json main.js jquery.js
+open -a node-webkit AdbTools.nw
